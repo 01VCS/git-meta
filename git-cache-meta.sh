eeb8a4ce48af032d48e0b82d19a689258a84b358
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-16 02:01:36 -0300

    for Mac OS X, `brew install findutils gawk coreutils`, by @bizonix
    https://gist.github.com/andris9/1978266#gistcomment-1448243
    
    Signed-off-by: Daniell Mesquita <daniellmesquita@protonmail.com>

--- git-cache-meta.sh
+++ git-cache-meta.sh
@@ -22,9 +22,23 @@
 #2014-03-18 - @the-mars: store properties for dirs too
 #2015-04-17 - time zone offset fallback; fix leading-dash-name error; avoid deeper find;
 #              better quote file names; better directory listing; merge short opts; by Danny Lin (@danny0838)
+#2015-05-07 - for Mac OS X, `brew install findutils gawk coreutils`, by @bizonix
 
 : ${GIT_CACHE_META_FILE=.git_cache_meta}
-: ${Tz:=$(find -prune -printf '%Tz')}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    GNU='g'
+fi
+for bin in find touch awk ; do
+    BIN=$( echo $bin | tr '[:lower:]' '[:upper:]')
+    eval ': ${'$BIN':=$(which $GNU$bin)}'
+    if [ "$GNU" == 'g' ] && ! [[ "${!BIN}" =~ /$GNU$bin ]]  ; then
+        echo "gnu version of '$bin' file not found." >&2
+        exit 1
+    fi
+done
+
+: ${Tz:=$($FIND -prune -printf '%Tz')}
 : ${Tz:=$(date +%z)}
 if ! [ "$Tz" ]; then
     echo "%z not supported in 'strftime' in C library." >&2
@@ -34,17 +48,17 @@ fi
 case $@ in
     --store|--stdout)
     case $1 in --store) exec > $GIT_CACHE_META_FILE; esac
-    { git ls-tree --name-only -rdz $(git write-tree) | xargs -0 -I NAME find ./NAME -maxdepth 0 \
+    { git ls-tree --name-only -rdz $(git write-tree) | xargs -0 -I NAME $FIND ./NAME -maxdepth 0 \
         \( -printf 'chown -h %U:%G \0%p\n' \) , \
         \( \! -type l -printf 'chmod %#m \0%p\n' \) , \
-        \( -printf 'touch -hcmd "%TY-%Tm-%Td %TH:%TM:%TS '$Tz'" \0%p\n' \) , \
-        \( -printf 'touch -hcad "%AY-%Am-%Ad %AH:%AM:%AS '$Tz'" \0%p\n' \)
-      git ls-files -z | xargs -0 -I NAME find ./NAME -maxdepth 0 \
+        \( -printf $TOUCH' -hcmd "%TY-%Tm-%Td %TH:%TM:%TS '$Tz'" \0%p\n' \) , \
+        \( -printf $TOUCH' -hcad "%AY-%Am-%Ad %AH:%AM:%AS '$Tz'" \0%p\n' \)
+      git ls-files -z | xargs -0 -I NAME $FIND ./NAME -maxdepth 0 \
         \( -printf 'chown -h %U:%G \0%p\n' \) , \
         \( \! -type l -printf 'chmod %#m \0%p\n' \) , \
-        \( -printf 'touch -hcmd "%TY-%Tm-%Td %TH:%TM:%TS '$Tz'" \0%p\n' \) , \
-        \( -printf 'touch -hcad "%AY-%Am-%Ad %AH:%AM:%AS '$Tz'" \0%p\n' \)
-    } | awk 'BEGIN {FS="\0"}; {print $1 "'\''" gensub(/'\''/, "'\''\\\\'\'''\''", "g", $2) "'\''" }' ;;
+        \( -printf $TOUCH' -hcmd "%TY-%Tm-%Td %TH:%TM:%TS '$Tz'" \0%p\n' \) , \
+        \( -printf $TOUCH' -hcad "%AY-%Am-%Ad %AH:%AM:%AS '$Tz'" \0%p\n' \)
+    } | $AWK 'BEGIN {FS="\0"}; {print $1 "'\''" gensub(/'\''/, "'\''\\\\'\'''\''", "g", $2) "'\''" }' ;;
     --apply) sh -e $GIT_CACHE_META_FILE;;
     *) 1>&2 echo "Usage: $0 --store|--stdout|--apply"; exit 1;;
 esac
