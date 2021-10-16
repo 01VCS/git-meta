1bf7cf1174013f5127c92a21163d76f7af7fafa1
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-16 01:17:07 -0300

    fix bugs of previous update and touch command as the LAST, by @brayrobert201 and reported by @Explorer09
    https://gist.github.com/andris9/1978266#gistcomment-812062
    https://gist.github.com/andris9/1978266#gistcomment-1176068
    
    Signed-off-by: Daniell Mesquita <daniellmesquita@protonmail.com>

--- git-cache-meta.sh
+++ git-cache-meta.sh
@@ -9,20 +9,23 @@
 
 #2012-03-05 - added filetime, by @andris9
 #Nov 29, 2013 - fix bug at failing on files with spaces, by @AntonioMeireles and reported by @kickiss
+#Feb 20, 2014 - fix bugs of previous update and touch command as the LAST, by @brayrobert201 and reported by @Explorer09
 pIFS=$IFS
-IFS=$'\n'
+
+IFS='
+'
 
 : ${GIT_CACHE_META_FILE=.git_cache_meta}
 case $@ in
---store|--stdout)
-case $1 in --store) exec > $GIT_CACHE_META_FILE; esac
-find $(IFS=$'\n' ; git ls-files)\
-\( -printf 'chown %U "%p"\n' \) \
-\( -printf 'chgrp %G "%p"\n' \) \
-\( -printf 'touch -c -d "%AY-%Am-%Ad %AH:%AM:%AS" "%p"\n' \) \
-\( -printf 'chmod %#m "%p"\n' \) ;;
---apply) sh -e $GIT_CACHE_META_FILE;;
-*) 1>&2 echo "Usage: $0 --store|--stdout|--apply"; exit 1;;
+    --store|--stdout)
+    case $1 in --store) exec > $GIT_CACHE_META_FILE; esac
+    find $(git ls-files)\
+    \( -printf 'chown %U "%p"\n' \) \
+    \( -printf 'chgrp %G "%p"\n' \) \
+    \( -printf 'chmod %#m "%p"\n' \) \
+    \( -printf 'touch -c -d "%AY-%Am-%Ad %AH:%AM:%AS" "%p"\n' \) ;;
+    --apply) sh -e $GIT_CACHE_META_FILE;;
+    *) 1>&2 echo "Usage: $0 --store|--stdout|--apply"; exit 1;;
 esac
 
 IFS=$pIFS
