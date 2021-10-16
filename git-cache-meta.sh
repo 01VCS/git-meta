a33f15269ba9209a5c5d9a87859318ff46991dda
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-10-16 01:30:19 -0300

    important modifications/fixes by @Explorer09
    https://gist.github.com/andris9/1978266#gistcomment-1178496
    
    Signed-off-by: Daniell Mesquita <daniellmesquita@protonmail.com>

--- git-cache-meta.sh
+++ git-cache-meta.sh
@@ -11,18 +11,33 @@
 #2013-11-29 - fix bug at failing on files with spaces, by @AntonioMeireles and reported by @kickiss
 #2014-02-20 - fix bugs of previous update and touch command as the LAST, by @brayrobert201 and reported by @Explorer09
 #2014-02-23 - fix bugs of previous updates, by @stefanbj
+#2014-02-24 - important modifications/fixes by @Explorer09:
+# - 'touch' commands are moved to the bottom.
+# - File modification time and access time are stored separately.
+# - Timezone offsets are stored. (Strictly, the %Tz and %Az things are not documented in GNU find, but they will work as long as you have a C99-complaint library.)
+# - Added '-h' switch to chown and chgrp. This allows the script to handle symlinks.
+# - 'chmod' only if the file is not a symlink.
+# - All unusual filenames are properly escaped, thanks to '-exec ls --quoting-style=shell'. Notice that '--quoting-style=c' does not work as it seems when there are filenames that contain newlines.
 
 IFS='
 '
 
 : ${GIT_CACHE_META_FILE=.git_cache_meta}
+
+if [ -n "$(find -prune -printf '%Tz %Az\n' | tr -d ' 0-9+-')" ]; then
+echo "%z not supported in 'strftime' in C library." >&2
+exit 1
+fi
+
 case $@ in
     --store|--stdout)
     case $1 in --store) exec > $GIT_CACHE_META_FILE; esac
-    git ls-files -z | xargs -0r -I '{}' find '{}' \
-    \( -printf 'chown %U:%G "%p"\n' \) \
-    \( -printf 'chmod %#m "%p"\n' \) \
-    \( -printf 'touch -c -d "%AY-%Am-%Ad %AH:%AM:%AS" "%p"\n' \) ;;
+    git ls-files -z | xargs -0 -I NAME find NAME \
+        \( -printf 'chown -h %U ' -exec ls --quoting-style=shell '{}' \; \) , \
+        \( -printf 'chgrp -h %G ' -exec ls --quoting-style=shell '{}' \; \) , \
+        \( \! -type l -printf 'chmod %#m ' -exec ls --quoting-style=shell '{}' \; \) , \
+        \( -printf 'touch -c -h -m -d "%TY-%Tm-%Td %TH:%TM:%TS %Tz" ' -exec ls --quoting-style=shell '{}' \; \) , \
+        \( -printf 'touch -c -h -a -d "%AY-%Am-%Ad %AH:%AM:%AS %Az" ' -exec ls --quoting-style=shell '{}' \; \) ;;
     --apply) sh -e $GIT_CACHE_META_FILE;;
     *) 1>&2 echo "Usage: $0 --store|--stdout|--apply"; exit 1;;
 esac
