02826e78ac6b8123a32c29b8a20be08e2648b4eb
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-11 21:59:54 -0300

    Better append of git hook

--- init.sh
+++ init.sh
@@ -15,12 +15,11 @@ fi
             then
                 echo ".git/hooks/pre-commit exists. Appending to it..."
                 cat >> .git/hooks/pre-commit <<EOF
-#!/bin/bash
-
 #01VCSHere
 
 echo "Storing files' timestamp and other metadata..." && bash .git/hooks/git-meta.sh --store && git add .gitmeta && echo "Done. Meta has been preserved!"
 EOF
+                
                 echo "Done!"
             else
                 echo ".git/hooks/pre-commit doesn't exist. Creating it..."
