b30cc716937f552771dd1f0aa71d9f314c1f03d4
Daniella Mesquita <mesquitadaniella@proton.me>
2023-09-30 18:13:58 -0300

    final test

--- init.sh
+++ init.sh
@@ -36,9 +36,9 @@ EOF
                 chmod +x .git/hooks/pre-commit
                 echo "Done!"
 fi
-        if [ ! -f ".gitmeta-cid" ]; then touch .gitmeta-cid; fi
+        if [ ! -f ".gitmeta-cid" ]; then touch .cid-gitmeta; fi
         echo "Done! git-meta has been initiated in your repo!"
     else
-        if [ ! -f ".gitmeta-cid" ]; then touch .gitmeta-cid; fi
+        if [ ! -f ".gitmeta-cid" ]; then touch .cid-gitmeta; fi
         echo "git-meta is already initiated in your repo."
 fi
